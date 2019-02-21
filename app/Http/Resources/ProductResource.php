d7f1f480994f31c58c3a4744f0355c9e18d748a0
Golam Sorwar <akibmd719@gmail.com>
2019-02-22 02:25:11 +0600

    Update calculation

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -20,8 +20,7 @@ class ProductResource extends JsonResource
           'price' => $this->price,
           'stock' => $this->stock == 0 ? 'Out of stock' :$this->stock,
           'discount' => $this->discount,
-          //'totalPrice' => round((1- ($this->discount/100)) * $this->price,2),
-          'totalPrice' => round(($this->discount/100) * $this->price,2),
+          'totalPrice' => round((1- ($this->discount/100)) * $this->price,2),
           /* totalPrice calculation system
           48/100=.48
           1- .48=.52

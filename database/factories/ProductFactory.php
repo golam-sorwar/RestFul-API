a884a5035d0e576018509c81ef7f5df539bf6c06
Golam Sorwar <akibmd719@gmail.com>
2019-02-21 22:00:14 +0600

    Database seeding with Faker library

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'details' => $faker->paragraph,
+        'price' =>$faker->numberBetween(250,2000),
+        'stock' => $faker->randomDigit,
+        'discount' =>$faker->numberBetween(20,80)
     ];
 });

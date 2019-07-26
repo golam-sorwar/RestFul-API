1f989babddcae46f6b36ea9e4df7c3a056d00aee
Golam Sorwar <akibmd719@gmail.com>
2019-07-26 21:50:24 +0600

    Update database and file location and update composer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -13,8 +13,35 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    @php
+                        $product=App\Product::paginate(8);
+                    @endphp
+                    {{-- You are logged in! <br> --}}
+                    <table class="table table-bordered">
+                        <thead class="thead-dark">
+                           <tr>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Discount</th>
+                                <th>Star</th>
+                            </tr> 
+                        </thead>
+                        <tbody>
+                            @foreach ($product as $element)
+                                <tr>
+                                    <td>{{ $element->name }}</td>
+                                    <td>{{ $element->price }}</td>
+                                    <td>{{ $element->discount }}</td>
+                                    <td>
+                                            @foreach ($element->reviews as $elements)
+                                                {{ $elements->star }} &nbsp 
+                                            @endforeach
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{ $product->links() }}
                 </div>
             </div>
         </div>
